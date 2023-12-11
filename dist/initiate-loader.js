5a6d8282f99500b1336a806154e515b169d220b7
Michael Lin <mikemlin@amazon.com>
2023-12-11 09:26:00 -0800

    main.js changes

--- dist/initiate-loader.js
+++ dist/initiate-loader.js
@@ -6,7 +6,6 @@
 
 // The ChatBotUiLoader variable contains the FullPageLoader field which is a
 // constructor for the loader.
-console.log("Here");
 var Loader = ChatBotUiLoader.FullPageLoader;
 
 // The loader constructor supports various configurable options used to
